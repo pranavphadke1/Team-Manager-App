9d41240cefabd56f9aba23069eece26f96037388
Pranav Phadke <pranavmphadke@gmail.com>
2023-03-09 19:53:46 -0500

    create Member model and migration

--- teamMembersManagement/team/models.py
+++ teamMembersManagement/team/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    
+    # Maybe change to role and enum later... 
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.first_name + '' + self.last_name
