3596168f76116f9ab0d12fff7f7a7548cc98ce34
Pranav Phadke <pranavmphadke@gmail.com>
2023-03-10 15:04:55 -0500

    remove custom form

--- teamMembersManagement/team/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-
-from .models import Member
-
-class MemberForm(forms.ModelForm):
-    class Meta:
-        model = Member
-        fields = '__all__'
