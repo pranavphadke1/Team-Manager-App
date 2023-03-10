95bbc1a2de02651a27c4fb8d535938279d5d9ce0
Pranav Phadke <pranavmphadke@gmail.com>
2023-03-10 15:01:26 -0500

    Switched to Class Based generic views

--- teamMembersManagement/team/urls.py
+++ teamMembersManagement/team/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from . import views
+from .views import ListTeam, updateMember, MemberList, MemberUpdate, MemberCreate, MemberDelete
 
 urlpatterns = [
-    path('', views.index, name='list'),
-    path('update_member/<str:pk>', views.updateMember, name='update_member')
+    path('', MemberList.as_view(), name='members'),
+    path('member-create/', MemberCreate.as_view(), name='member-create'),
+    path('member-update/<str:pk>/', MemberUpdate.as_view(), name='member-update'),
+    path('member-delete/<str:pk>/', MemberDelete.as_view(), name='member-delete'),
+    # path('', ListTeam, name='list'),
+    # path('update_member/<str:pk>', updateMember, name='update_member')
 ]
+
+# MemberList.as_view()
--- teamMembersManagement/team/views.py
+++ teamMembersManagement/team/views.py
@@ -1,11 +1,43 @@
 from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Member
 from .forms import MemberForm
 
 # Create your views here.
 
-def index(request):
+
+class MemberList(ListView):
+    model = Member 
+    context_object_name = 'members'
+    template_name = 'team/member_list.html'
+
+class MemberCreate(CreateView):
+    model = Member
+    fields = '__all__'
+    success_url = reverse_lazy('members')
+    template_name = 'team/member_create.html'
+    
+
+class MemberUpdate(UpdateView):
+    model = Member
+    fields = '__all__'
+    success_url = reverse_lazy('members')
+    template_name = 'team/member_update.html'
+
+class MemberDelete(DeleteView):
+    model = Member
+    context_object_name = 'member'
+    success_url = reverse_lazy('members')
+    template_name = 'team/member_delete.html'
+    
+
+
+
+def ListTeam(request):
     members = Member.objects.all()
     form = MemberForm()
 
