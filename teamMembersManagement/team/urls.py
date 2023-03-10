3e88509320bf8d1021d118093f09e18d378e2f80
Pranav Phadke <pranavmphadke@gmail.com>
2023-03-10 15:04:38 -0500

    remove old function views

--- teamMembersManagement/team/urls.py
+++ teamMembersManagement/team/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import ListTeam, updateMember, MemberList, MemberUpdate, MemberCreate, MemberDelete
+from .views import MemberList, MemberUpdate, MemberCreate, MemberDelete
 
 urlpatterns = [
     path('', MemberList.as_view(), name='members'),
     path('member-create/', MemberCreate.as_view(), name='member-create'),
     path('member-update/<str:pk>/', MemberUpdate.as_view(), name='member-update'),
     path('member-delete/<str:pk>/', MemberDelete.as_view(), name='member-delete'),
-    # path('', ListTeam, name='list'),
-    # path('update_member/<str:pk>', updateMember, name='update_member')
 ]
-
-# MemberList.as_view()
--- teamMembersManagement/team/views.py
+++ teamMembersManagement/team/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Member
-from .forms import MemberForm
 
 # Create your views here.
-
 
 class MemberList(ListView):
     model = Member 
@@ -33,38 +29,3 @@ class MemberDelete(DeleteView):
     context_object_name = 'member'
     success_url = reverse_lazy('members')
     template_name = 'team/member_delete.html'
-    
-
-
-
-def ListTeam(request):
-    members = Member.objects.all()
-    form = MemberForm()
-
-    if request.method == 'POST':
-        form = MemberForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-
-    context = {'members': members, 'form': form}
-    return render(request, 'team/list.html', context)
-
-def updateMember(request, pk):
-    member = Member.objects.get(id = pk)
-    form = MemberForm(instance = member)
-
-    if request.method == 'POST':
-        form = MemberForm(request.POST, instance=member)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-    
-    # elif request.method == 'DELETE':
-    #     print("HELLO WORLD")
-    #     member.delete()
-    #     return redirect('/')
-
-
-    context = {'form': form}
-    return render(request, 'team/update_member.html', context)
