e2a8deea048514ab993d6451ebcc4111670118fa
Pranav Phadke <pranavmphadke@gmail.com>
2023-03-10 02:50:32 -0500

    Update through post

--- teamMembersManagement/team/urls.py
+++ teamMembersManagement/team/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='list'),
+    path('update_member/<str:pk>', views.updateMember, name='update_member')
 ]
--- teamMembersManagement/team/views.py
+++ teamMembersManagement/team/views.py
@@ -17,3 +17,22 @@ def index(request):
 
     context = {'members': members, 'form': form}
     return render(request, 'team/list.html', context)
+
+def updateMember(request, pk):
+    member = Member.objects.get(id = pk)
+    form = MemberForm(instance = member)
+
+    if request.method == 'POST':
+        form = MemberForm(request.POST, instance=member)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    # elif request.method == 'DELETE':
+    #     print("HELLO WORLD")
+    #     member.delete()
+    #     return redirect('/')
+
+
+    context = {'form': form}
+    return render(request, 'team/update_member.html', context)
